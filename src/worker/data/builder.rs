3eb81cd9fd412d070ae1698f4cb99faf585c5ccf
Stanislav Bohm <spirali@kreatrix.org>
2018-03-31 12:31:40 +0200

    WIP

--- src/worker/data/builder.rs
+++ src/worker/data/builder.rs
@@ -3,20 +3,36 @@ use super::data::{Data, Storage};
 use errors::Result;
 use super::super::fs::workdir::WorkDir;
 use common::DataType;
+use worker::fs::tempfile::TempFileName;
+
+enum BuilderStorage {
+    Memory(Vec<u8>),
+    File((File, TempFileName))
+}
 
 pub struct DataBuilder {
-    buffer: Vec<u8>,
+    storage: BuilderStorage,
     data_type: DataType,
 }
 
 impl DataBuilder {
-    pub fn new(_workdir: &WorkDir, data_type: DataType, expected_size: Option<usize>) -> Self {
-        let mut buffer = Vec::new();
-        // TODO: If size is to big, redirect to disk
-        if let Some(size) = expected_size {
-            buffer.reserve(size);
+    pub fn new(workdir: &WorkDir, data_type: DataType, expected_size: Option<usize>) -> Self {
+
+        fn file_storage(workdir: &WorkDir) -> BuilderStorage {
+            let f = workdir.make_temp_file();
+            BuilderStorage::File((File::open(f.path()).unwrap(), f))
         }
-        DataBuilder { data_type, buffer }
+
+        let storage = if let Some(size) = expected_size {
+            if size < 256 * 1024 {
+                BuilderStorage::Memory(Vec::with_capacity(size))
+            } else {
+                file_storage(workdir)
+            }
+        } else {
+            file_storage(workdir)
+        };
+        DataBuilder { data_type, storage }
     }
 
     // TODO: Get rid of this method
@@ -33,13 +49,18 @@ impl DataBuilder {
     }
 
     pub fn write(&mut self, data: &[u8]) {
-        self.buffer.extend_from_slice(data);
+        match self.storage {
+            BuilderStorage::Memory(ref mut buffer) => buffer.extend_from_slice(data),
+            BuilderStorage::File(ref file, _) => file.write_all(data),
+        }
     }
 
     pub fn build(&mut self) -> Data {
-        Data::new(
-            Storage::Memory(::std::mem::replace(&mut self.buffer, Vec::new())),
-            self.data_type,
-        )
+        match self.storage {
+            BuilderStorage::Memory(ref mut buffer) => Data::new(
+                Storage::Memory(::std::mem::replace(buffer, Vec::new())),
+                self.data_type,
+            )
+        }
     }
 }
